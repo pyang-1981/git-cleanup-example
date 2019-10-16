c509084429f715957fc32deeed0e069641dfda68
Peng Yang <ppp.yang@gmail.com>
2019-10-16 13:16:14 -0700

    Added initial files.

---
+++ gen_random_blob.py
@@ -0,0 +1,14 @@
+#/bin/env python3
+
+import os
+import sys
+import uuid
+
+def main():
+    fname = "blob-%s.bin" % uuid.uuid4() 
+    with open(fname, 'wb') as fp:
+        fp.write(os.urandom(1024))
+
+if __name__ == "__main__":
+    main()
+
